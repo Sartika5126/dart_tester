94474d39cf1327f61d9e263e7ae68d3221211ae5
unknown <sartika.lp3i.krw24@gmail.com>
2025-11-13 22:31:28 +0700

    lanjutan tester_dart

--- bin/dart_tester.dart
+++ bin/dart_tester.dart
@@ -1,11 +1,33 @@
-import 'package:dart_tester/helper/produk_helper.dart';
-import 'package:dart_tester/helper/user_helper.dart' as users;
+import 'package:dart_tester/helper/user_helper.dart';
 import 'package:dart_tester/model/user.dart';
 
 void main(List<String> arguments) async {
-  // ambilProduk();
-  print("Ambil data dari user...");
-  User pengguna = await users.getUserById(5);
-  print(pengguna.email);
+  print("Sedang mengambil data...");
+  List<dynamic> coba = await UserHelper().getAllUser();
+  print(coba[0].username);
 
+  // membuat objek untuk menampung data user
+  User pengguna = await UserHelper().getUserById(2);
+  print(pengguna.username);
+  print("Email :${pengguna.email}");
+
+  // membuat user baru 
+  User p = User(
+    id: 10, 
+    username: "sartika", 
+    email: "sartika@gmail.com", 
+    password: "051206");
+  // kirim data ke endpoint
+  int idUserBaru = await UserHelper().creatUser(p);
+  print("id user yang baru dibuat :$idUserBaru");
+
+  // mengupdate user baru
+  User up = User(
+    id: 2, 
+    username: "dzakwan", 
+    email: "dzakwan@gmail.com", 
+    password: "22025");
+    //simpan hasil respon
+    User hasil = await UserHelper().updateUser(up);
+    print("Ini hasil update : $hasil");
 }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+// import 'dart:convert';
 
 class User {
   final int id;
@@ -12,20 +12,8 @@ class User {
     required this.password,
   });
 
-  User copyWith({
-    int? id,
-    String? username,
-    String? email,
-    String? password,
-  }) {
-    return User(
-      id: id ?? this.id,
-      username: username ?? this.username,
-      email: email ?? this.email,
-      password: password ?? this.password,
-    );
-  }
-
+  // constructor, conversi dari atau ke json
+  
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -43,10 +31,6 @@ class User {
       password: map['password'] ?? '',
     );
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory User.fromJson(String source) => User.fromMap(json.decode(source));
 
   @override
   String toString() {
